6f077094afd41afeab8e26d716e2b5233ef40ea7
Adrien <102225551+M-Adrien74@users.noreply.github.com>
2022-08-16 17:10:36 +0200

    :sparkles: /-Creation de la vue "ajouter utilisateur"/-Creation du formulaire/-Ajout en BDD. (a faire, la redirection => redirectToRoute.) (#6)
    Co-authored-by: Caliendo Julien <34681928+caliendojulien@users.noreply.github.com>

---
+++ src/Controller/AjouterStagiaireController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateurs;
+use App\Form\AjoutStagiaireType;
+use App\Repository\UtilisateursRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AjouterStagiaireController extends AbstractController
+{
+    #[Route('/ajouter', name: 'app_ajouter')]
+    public function ajouter(
+        Request                $request,
+        EntityManagerInterface $em,
+
+    ): Response
+    {
+
+        $ajoutStagiaire = new Utilisateurs();
+        $ajoutStagiaireForm = $this->createForm(AjoutStagiaireType::class, $ajoutStagiaire);
+        $ajoutStagiaireForm->handleRequest($request);
+        if ($ajoutStagiaireForm->isSubmitted() && $ajoutStagiaireForm->isValid()) {
+            $ajoutStagiaire->setIsAdmin(false);
+            $em->persist($ajoutStagiaire);
+            $em->flush();
+            return $this->redirectToRoute();
+        }
+        return $this->renderForm('ajouter_stagiaire/ajouter.html.twig', compact('ajoutStagiaireForm'));
+    }
+}
+
